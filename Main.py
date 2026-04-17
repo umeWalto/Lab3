23e0b148029b8be61429ff34dad1b923f213047f
N.Myzgin <nma_university@mail.ru>
2026-04-17 21:43:35 +0500

    Финальная версия

--- Main.py
+++ Main.py
@@ -3,41 +3,26 @@ from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QPainter, QColor, QPen
 from PyQt6.QtCore import Qt
 
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Круги на форме")
-        self.setGeometry(100, 100, 800, 600)
-
-        self.storage = MyStorage()
-
-        def paintEvent(self, event):
-            painter = QPainter(self)
-            self.storage.draw_all(painter)
-        
-    def mousePressEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            modifiers = QApplication.keyboardModifiers()
-            ctrl_pressed = bool(modifiers & Qt.KeyboardModifier.ControlModifier)
-
-            self.storage.handle_click(
-                int(event.position().x()),
-                int(event.position().y()),
-                ctrl_pressed
-            )
-            self.update()
-    
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key.Key_Delete:
-            self.storage.remove_selected()
-            self.update()
-
 class CCircle:
     def __init__(self, x, y, radius=30):
         self._x = x
         self._y = y
         self._radius = radius
         self._selected = False
+
+    def draw(self, painter: QPainter):
+        if self._selected:
+            painter.setBrush(QColor(255, 100, 100))
+        else:
+            painter.setBrush(QColor(100, 200, 255))
+
+        painter.setPen(QPen(Qt.GlobalColor.black, 2))
+        painter.drawEllipse(
+            self._x - self._radius,
+            self._y - self._radius,
+            self._radius * 2,
+            self._radius * 2
+        )
 
     def contains(self, x, y):
         dx = x - self._x
@@ -49,20 +34,6 @@ class CCircle:
 
     def is_selected(self):
         return self._selected
-
-def draw(self, painter: QPainter):
-    if self._selected:
-        painter.setBrush(QColor(255, 100, 100))
-    else:
-        painter.setBrush(QColor(100, 200, 255))
-
-    painter.setPen(QPen(Qt.GlobalColor.black, 2))
-    painter.drawEllipse(
-        self._x - self._radius,
-        self._y - self._radius,
-        self._radius * 2,
-        self._radius * 2
-    )
 
 class MyStorage:
     def __init__(self):
@@ -83,7 +54,7 @@ class MyStorage:
 
     def get_object(self):
         return self._data[self._index]
-    
+
     def draw_all(self, painter: QPainter):
         self.first()
         while not self.eol():
@@ -105,14 +76,23 @@ class MyStorage:
             self.clear_selection()
 
         if clicked:
-            for obj in clicked:
+            SELECT_ALL = True
+
+            if SELECT_ALL:
+                for obj in clicked:
+                    if ctrl_pressed:
+                        obj.set_selected(not obj.is_selected())
+                    else:
+                        obj.set_selected(True)
+            else:
+                obj = clicked[-1]
                 if ctrl_pressed:
                     obj.set_selected(not obj.is_selected())
                 else:
                     obj.set_selected(True)
         else:
             self.add(CCircle(x, y))
-        
+
     def clear_selection(self):
         self.first()
         while not self.eol():
@@ -130,6 +110,39 @@ class MyStorage:
             self.next()
 
         self._data = new_data
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Круги на форме")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.storage = MyStorage()
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        self.storage.draw_all(painter)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            modifiers = QApplication.keyboardModifiers()
+            ctrl_pressed = bool(modifiers & Qt.KeyboardModifier.ControlModifier)
+
+            self.storage.handle_click(
+                int(event.position().x()),
+                int(event.position().y()),
+                ctrl_pressed
+            )
+            self.update()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Delete:
+            self.storage.remove_selected()
+            self.update()
+
+    def resizeEvent(self, event):
+        self.update()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
