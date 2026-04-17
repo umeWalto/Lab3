5c79c7742fa64d2b59a4e1d55f5b08243085dcc7
N.Myzgin <nma_university@mail.ru>
2026-04-17 17:26:30 +0500

    Создан класс CCircle с геометрией фигуры и селектором

--- Main.py
+++ Main.py
@@ -7,6 +7,24 @@ class MainWindow(QWidget):
         self.setWindowTitle("Круги на форме")
         self.setGeometry(100, 100, 800, 600)
 
+class CCircle:
+    def __init__(self, x, y, radius=30):
+        self._x = x
+        self._y = y
+        self._radius = radius
+        self._selected = False
+
+    def contains(self, x, y):
+        dx = x - self._x
+        dy = y - self._y
+        return dx * dx + dy * dy <= self._radius * self._radius
+
+    def set_selected(self, value: bool):
+        self._selected = value
+
+    def is_selected(self):
+        return self._selected
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
